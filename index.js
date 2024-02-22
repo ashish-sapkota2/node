bcaf6882af066f498248dadb46dd96d6766ed4bb
Ashish Sapkota <aryanashish223@gmail.com>
2024-02-22 11:36:20 +0545

    .

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express')
 const anything = require("./second")
+
+const app = express();
+app.get('/',function(req,res){
+    res.send("hello there")
+})
+app.listen(3000,()=>{
+    console.log("Server staarted on : 3000");
+})
 
 console.log("Hello ", anything.name, "Your current status :" , anything.status) 
---
+++ users.js
@@ -0,0 +1,22 @@
+const user =[
+    {
+    id : 1,
+    name: 'Ashish',
+    email: 'aryanashish223@gmail.com'
+},
+{
+    id : 2,
+    name: 'hari',
+    email: 'harish@gmail.com'
+},
+{
+    id : 6,
+    name: 'sita',
+    email: 'sita@gmail.com'
+},
+{
+    id : 2,
+    name: 'shyam',
+    email: 'sh123@gmail.com'
+},
+]
