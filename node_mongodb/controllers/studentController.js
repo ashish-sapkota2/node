b6e7f7340c3d0619989768bcbe36bb66af0e58a6
ashish <aryanashish223@gmail.com>
2024-03-18 13:36:37 +0545

    db

---
+++ node_mongodb/controllers/studentController.js
@@ -0,0 +1,78 @@
+const express = require ('express')
+var router =express.Router()
+const mongoose = require('mongoose')
+const Student = mongoose.model('Student')
+
+router.get('/',(req,res)=>{
+    res.render('student/addOredit'),{
+        viewTitle : 'Insert Student'
+    }
+})
+
+router.post ('/', (req, res)=>{
+    if(res.body._id== ''){
+        insertRecord(req,res)
+    }else{
+        updateRecord(req,res)
+    }
+})
+
+function insertRecord(req,res){
+    var student = new Student()
+    student.fullName = req.body.fullName;
+    student.email = req.body.email;
+    student.mobile = req.bosy.mobile;
+    student.city = req.body.city;
+    student.save((err,doc)=>{
+        if(!eerr){
+            res.redirect('student/list')
+        }else{
+            console.log('error during insert : '+err)
+        }
+    })
+}
+
+function updateRecord(req,res){
+    student.findOneAndUpdate({_id: req.body._id}, req.body , {new: true},(err,doc)=>{
+        if(!err){
+            res.redirect('student/list')
+        }else{
+            console.log('error during update: ' +err)
+        }
+    });
+}
+
+router.get('/list',(req,res)=>{
+    Student.find((err,docs)=>{
+        if(!err){
+            res.render('student/list',{
+                list :docs
+            })
+        }else{
+            console.log('error while retriving: '+err)
+        }
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    Student.findById(req.params.id,(err,doc)=>{
+        if(!err){
+            res.render("student/addOredit",{
+                viewTitle : "update successful",
+                student: doc,
+            });
+            console.log(doc);
+        }
+    });
+});
+
+router.get('delete/:id',(req,res)=>{
+    Student.findByIdAndRemover(req.params.id,(err,doc)=>{
+        if(!err){
+            res.redirect("student/list");
+        }else{
+            console.log("Error in deletion " +err);
+        }
+    });
+});
+
---
+++ node_mongodb/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const handle = require('handlebars')
+const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyparser = require('body-parser')
+var app = express()
+
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
+
+app.get('/',(req,res)=>{
+    res.send(`
+    <h2>Welcome to students database !! </h2>
+    <h3> Click here to get access to the <b><a href ="/student/list">Database</a></b><h3>`)
+});
+
+app.set('views', path.join(__dirname, '/views/'))
+
+app.engine('hbs', 
+    exphbs({
+    handlebars : 'hbs',
+    defaultLayout : 'MainLayout',
+    layoutsDir : __dirname + '/views/layouts/'
+
+})
+);
+
+app.set("view engine", "hbs");
+
+app.listen(3000,()=>{
+    console.log("server started at port 3000");
+})
